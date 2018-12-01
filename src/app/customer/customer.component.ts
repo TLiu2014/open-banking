6ef4fd3554d0dfaecc2a3ddc801e9ce4e8811376
Tianwei Liu <TianweiLiu2012@gmail.com>
2018-12-01 14:20:14 -0500

    Setup API and add customer component.

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({
+    // 'Content-Type':  'application/json', 
+    'Ocp-Apim-Subscription-Key': '5b47e3170d1b42688dbe1474941f4060'
+  })
+};
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.scss']
+})
+export class CustomerComponent implements OnInit {
+
+  customers: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://api.azureminilab.com/customers/', httpOptions)
+      .subscribe(res => {
+        console.log(res);
+        this.customers = res;
+      });
+  }
+
+}
