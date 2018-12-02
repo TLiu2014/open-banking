9a5d465bdd9159f3dd713e1864f3ac846fea7631
Pia <lee240you@gmail.com>
2018-12-02 02:58:50 -0500

    Updated

---
+++ src/app/addgoal/addgoal.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+//import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-addgoal',
+  templateUrl: './addgoal.component.html',
+  styleUrls: ['./addgoal.component.scss']
+})
+export class AddgoalComponent implements OnInit {
+  //form: FormGroup;
+  //constructor(private formBuilder: FormBuilder ) {  }
+
+  ngOnInit() {
+    //this.form = this.formBuilder.group({
+    //name: new FormControl(null, Validators.required),
+    //amount: new FormControl(null, Validators.required)});
+  
+}
+
+  
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {MatCardModule} from '@angular/material';
+import { MatCardModule} from '@angular/material';
+import { AddgoalComponent } from './addgoal/addgoal.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
     DashboardComponent,
+    AddgoalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatCardModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
